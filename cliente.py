8d4c4d1e3bf072de8abae914a0bc84fd2d7d5906
matheus pereira <matheper@gmail.com>
2012-04-22 18:52:20 -0300

    Add send and recv files

--- cliente.py
+++ cliente.py
@@ -6,26 +6,75 @@ import os
 
 class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
 
+#    def sendFile():
+#        music=open(fileName,'rb')
+#
+#        print "enviado %s"%(fileName)
+#        for i in music.readlines():
+#            self.tcp.send(i)
+#
+#        music.close()
+#
+#    def recvFile():
+#        fileName = connection.recv(1024)
+#        print "Recebendo %s"%(fileName)
+#        music = open(fileName+"receved.mp3",'wb')
+#        while 1:
+#            data=connection.recv(1024)
+#            if not data:
+#                break
+#            music.write(data)
+#        music.close()
+
+    def processMessage(self, message):
+        """Process the message according to the type."""
+        data = eval(message)
+        messageType = data['type']
+        if messageType == 3:#file transfer
+            filename = data['filename']
+            print 'Enviando arquivo %s.'%(filename)
+            File = open(directory+'/'+filename,'rb')
+            for i in File.readlines():
+                self.request.sendall(i)
+            File.close()
+            print 'Enviado com sucesso.'
+#            self.request.sendall('teste')
+
     def handle(self):
-        data = self.request.recv(1024)
-
-        """Trata mensagem recebida conforme tipo, é o lado servidor."""
-
+        message = self.request.recv(1024)
+        self.processMessage(message)
         cur_thread = threading.current_thread()
-        response = "{}: {}".format(cur_thread.name, data)
-        self.request.sendall(response)
 
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     pass
 
+def recvFile(ip,port,message):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((ip,port))
+    filename = message['filename']
+    try:
+        print "Recebendo %s"%(filename)
+        data = str(message)
+        sock.send(data)
+        File = open(directory+'/'+filename,'wb')
+        while 1:
+            data=sock.recv(1024)
+            if not data:
+                break
+            File.write(data)
+        File.close()
+        return "Arquivo Recebido com sucesso."
+    finally:
+        print "aqui"
+        sock.close()
 
-def client(ip, port, message):
+def sendMessage(ip, port, message):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((ip, port))
     try:
         sock.sendall(message)
         response = sock.recv(1024)
-        print "Received: {}".format(response)
+        return response
     finally:
         sock.close()
 
@@ -39,16 +88,64 @@ if __name__ == "__main__":
     server_thread.daemon = True
     server_thread.start()
 
-    files = os.listdir(os.path.expanduser('~/Arquivos/CienciaDaComputacao/6.Semestre/Sistemas_Distribuidos/napster'))
-    message={}
-    message['type']= 'connectToServer'
-    message['ip']=ip
-    message['port']= port
-    message['files']= files
+    directory = raw_input("Digite o diretório para compartilhar arquivos: ")
 
-    data = str(message)
+    outputMessage = "Digite:\n1:Conectar ao Servidor\n"
+    outputMessage += "2:Consultar\n3:Teste\n"
+    outputMessage += "50:Desconectar do Servidor\n0:Sair\n"
+    userOpt = int(raw_input(outputMessage))
 
-    client(ip, 8004, data)
+    while userOpt:
+        if userOpt == 1:
+            files = os.listdir(os.path.expanduser(directory))
+            message={}
+            message['type']= 0 #connect server
+            message['ip']=ip
+            message['port']= port
+            message['files']= files
 
-    server.serve_forever()
-#    server.shutdown()
+            data = str(message)
+
+            sendMessage(ip, 8004, data)
+
+        elif userOpt == 50:
+            message = {}
+            message['type']= 1 #disconnect server
+            message['ip']= ip
+            message['port']= port
+
+            data = str(message)
+            sendMessage(ip, 8004, data)
+
+        if userOpt == 2:
+            filename = raw_input("Digite o nome do arquivo: ");
+            message={}
+            message['type']=2 #file search
+            message['ip']=ip
+            message['port']=port
+            message['filename']=filename
+
+            data = str(message)
+            response = eval(sendMessage(ip,8004,data))
+            if not response:
+                print "Arquivo não encontrado."
+            else:
+                index = 1;
+                for opt in response:
+                    print "id: %s, local: %s"%(index,opt)
+                    index += 1
+                index = int(raw_input("Digite o id do arquivo escolhido:"))
+
+                serv = response[index-1]
+
+                message = {}
+                message['type']=3 #file transfer
+                message['filename']=filename
+                
+                response = recvFile(serv[0],serv[1],message)
+
+                print response
+
+        userOpt = int(raw_input(outputMessage))
+
+    server.shutdown()
--- servidor.py
+++ servidor.py
@@ -3,15 +3,40 @@ import socket
 import threading
 import SocketServer
 import os
+import time
 
 clientsList = {}
 
 class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
 
+    def processMessage(self, message):
+        """Process the message according to the type."""
+        data = eval(message)
+        messageType = data['type']
+        if messageType == 0:#connect
+            clientsList[(data['ip'],data['port'])]=(data['files'])
+            print clientsList
+            print "Added client %s port %s"%(data['ip'],data['port'])
+        elif messageType == 1:#disconnect
+            clientsList.pop((data['ip'],data['port']))
+            print "Removed client %s port %s"%(data['ip'],data['port'])
+        elif messageType == 2:#search
+            filename = data['filename']
+            response = []
+            for client in clientsList:
+                print "filename: %s"%(filename)
+                print "listFiles: %s"%(clientsList[client])
+                if filename in clientsList[client]:
+                    response.append(client)
+                    print 'Igual'
+            response = str(response)
+            self.request.sendall(response)
+
+
     def handle(self):
-        data = self.request.recv(1024)
-        data = eval(data)
-        """Trata mensagem recebida conforme tipo, é o lado servidor."""
+        message = self.request.recv(1024)
+        self.processMessage(message)
+        data = eval(message)
 
         cur_thread = threading.current_thread()
 #        response = "{}: {}".format(cur_thread.name, data)
@@ -40,4 +65,22 @@ if __name__ == "__main__":
     server_thread.daemon = True
     server_thread.start()
     print "Server loop running in thread:", server_thread.name
-    server.serve_forever()
+    
+    while True:
+        time.sleep(2)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            client = ()
+            for i in clientsList:
+                client = i
+                sock.connect(client)
+                message = {}
+                message['type'] = 4 #teste conexao
+                data = str(message)
+                sock.sendall(data)
+                print client
+        except:
+            clientsList.pop(i)
+            print 'Cliente %s desconectado por estar off line.'%(i[0])
+        finally:
+            sock.close()
